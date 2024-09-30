4308db2ec68ae769f874721b65720c7e02e066c0
yash gupta <150003197+yash-264@users.noreply.github.com>
2024-09-30 14:52:58 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,11 +1,24 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
+def isValid(s: str) -> bool:
     
-};
-
-module.exports = { isValid };
+    stack = []
+    
+    
+    bracket_map = {')': '(', '}': '{', ']': '['}
+    
+    
+    for char in s:
+        
+        if char in bracket_map:
+            
+            top_element = stack.pop() if stack else '#'
+            
+            if bracket_map[char] != top_element:
+                return False
+        else:
+            
+            stack.append(char)
+    
+    
+    return not stack
 
 
